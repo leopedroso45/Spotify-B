5f356ff56b3f91aa147560c9e845d5e26d5c8862
Leonardo Severo <leopedroso45@gmail.com>
2020-10-18 03:22:12 -0300

    Authenticating with spotify, it's necessary to save the token.

--- main.go
+++ main.go
@@ -4,57 +4,66 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/zmb3/spotify"
+	"html/template"
 	"log"
 	"net/http"
-	"os"
+	"path"
 )
 
 const redirectURI = "http://localhost:8080/callback"
 
 var (
-	auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
-	ch    = make(chan *spotify.Client)
-	state = "abc123"
+	auth = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
+
+	//TODO: randomize it
+	state = "state"
 )
 
 func main() {
-	id := os.Getenv("SPOTIFY_ID")
-	secret := os.Getenv("SPOTIFY_SECRET")
-	auth.SetAuthInfo(id, secret)
-	fmt.Println(id) ///////
+	auth.SetAuthInfo("", "")
+
 	router := mux.NewRouter()
-	router.HandleFunc("/callback", completeAuth)
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got request for:", r.URL.String())
-	})
-	go http.ListenAndServe(":8080", router)
+	router.HandleFunc("/", handleHome)
+	router.HandleFunc("/login", HandleLogin)
+	router.HandleFunc("/callback", HandleCallback)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
 
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	layout := path.Join("template", "home", "layout.html")
+	frontpage := path.Join("template", "home", "index.html")
+	tmpl, err := template.ParseFiles(layout, frontpage)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func HandleLogin(w http.ResponseWriter, r *http.Request) {
 	url := auth.AuthURL(state)
-	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
 
-	// wait for auth to complete
-	client := <-ch
-
-	// use the client to make calls that require authorization
+func HandleCallback(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("state") != state {
+		fmt.Println("State is not valid")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	token, err := auth.Token(state, r)
+	if err != nil {
+		fmt.Printf("Couldn't get token: %s\n", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	// use the token to get an authenticated client
+	client := auth.NewClient(token)
 	user, err := client.CurrentUser()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("You are logged in as:", user.ID)
-}
-
-func completeAuth(w http.ResponseWriter, r *http.Request) {
-	tok, err := auth.Token(state, r)
-	if err != nil {
-		http.Error(w, "Couldn't get token", http.StatusForbidden)
-		log.Fatal(err)
-	}
-	if st := r.FormValue("state"); st != state {
-		http.NotFound(w, r)
-		log.Fatalf("State mismatch: %s != %s\n", st, state)
-	}
-	// use the token to get an authenticated client
-	client := auth.NewClient(tok)
-	fmt.Fprintf(w, "Login Completed!")
-	ch <- &client
+	fmt.Fprintf(w, "Login Completed %s!\n <img src='%s'>", user.DisplayName, user.Images[0].URL)
 }
