c7f099696a0f689100977f3de718fb276b7799f3
Leonardo Severo <leopedroso45@gmail.com>
2020-10-17 23:06:44 -0300

    Adding oauth function.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/leopedroso45/spotify-b
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/zmb3/spotify v0.0.0-20200814173021-9bec46940cc0 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/zmb3/spotify"
+	"log"
 	"net/http"
+	"os"
+)
+
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
 )
 
 func main() {
+	id := os.Getenv("SPOTIFY_ID")
+	secret := os.Getenv("SPOTIFY_SECRET")
+	auth.SetAuthInfo(id, secret)
+	fmt.Println(id) ///////
 	router := mux.NewRouter()
-	router.HandleFunc("/", nil)
-	http.Handle("/", router)
+	router.HandleFunc("/callback", completeAuth)
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go http.ListenAndServe(":8080", router)
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	// wait for auth to complete
+	client := <-ch
+
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
+}
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+	// use the token to get an authenticated client
+	client := auth.NewClient(tok)
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- &client
 }
